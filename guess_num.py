cbac7c5269d3cd3aae8adc70df8a565493d90377
PSTkachev <P.S.Tkachev@gmail.com>
2021-02-03 20:05:03 +0300

    половинчатое деление

--- guess_num.py
+++ guess_num.py
@@ -1,5 +1,7 @@
 import random
 num = random.randint(1, 100)
+"""
+#ищем ручками
 while True:
     print('Угадайте число от 1 до 100')
     guess = input()
@@ -12,3 +14,26 @@ while True:
         print('Загаданное число больше')
     elif i > num:
         print('Загаданное число меньше')
+"""
+
+
+#реализуем поиск ответа методом половинчатого деления
+a = 1
+b = 100
+x = (b - a + 1) / 2
+
+while x != num:
+    if num < x:
+        print('Загаданное число меньше чем', x)
+        b = x
+        x = round(((b - a) / 2) + a)
+        print(x)
+
+    elif num > x:
+        print('Загаданное число больше чем', x)
+        a = x
+        x = round((b + a ) / 2)
+        print(x)
+
+if num == x:
+    print('Правильно!', x)
